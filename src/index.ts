3c314a5aa2ef63f3d52889fce2b0c9edaf08a6ce
Isha Mandaviya <mandviyaisha@gmail.com>
2025-11-27 10:29:00 +0530

    Release version 0.4.0 with updated documentation

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ export * from "./removeDirectory";
 export * from "./copyPath";
 export * from "./move";
 export * from "./getXandeumResult";
-export * from "./assignCoowner";
 export {exists } from "./exists";
 export {listDirectoryEntry} from "./listDirectoryEntery";
 export {getMetadata} from "./getMetadata";
